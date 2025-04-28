d8863ecaf59f432ddac1a7670116c6d3f94052b5
nnivxix <adhe.hanasa@gmail.com>
2025-04-28 17:34:02 +0700

    feat: add form

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
 import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
 </script>
 <template>
   <div>
-    <Button>hello</Button>
+    <form class="grid grid-cols-8 my-4 mx-auto max-w-2xl gap-4">
+      <Input class="col-span-7" />
+
+      <div class="col-span-1">
+        <Button>Submit</Button>
+      </div>
+    </form>
   </div>
 </template>
