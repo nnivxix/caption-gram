bb1b8f0db31d09b19780268615d3f1870ab907e1
nnivxix <adhe.hanasa@gmail.com>
2025-05-15 14:38:07 +0700

    feat: content component

---
+++ src/components/Content.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { defineModel } from "vue";
+import { Button } from "./ui/button";
+import { useClipboard } from "@vueuse/core";
+
+const value = defineModel<string>("value");
+
+const { copy, copied } = useClipboard();
+</script>
+<template>
+  <div class="flex-1 mx-4">
+    <div
+      v-if="value"
+      class="bg-muted rounded-2xl py-6 relative whitespace-pre-line mx-auto px-4 max-w-2xl"
+    >
+      <Button class="absolute top-4 right-4" @click="copy(value)">
+        {{ copied ? "Copied!" : "Copy" }}
+      </Button>
+      {{ value }}
+    </div>
+  </div>
+</template>
