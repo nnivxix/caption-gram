2278bb7f909564c06fac94286b64d362f8ed3c5c
nnivxix <adhe.hanasa@gmail.com>
2025-05-15 14:33:08 +0700

    feat footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="rounded-2xl my-4 max-w-2xl mx-auto w-full">
+    <div
+      class="text-center text-sm py-4 space-y-2 rounded-2xl bg-muted text-muted-foreground"
+    >
+      <p>HeadScout - 2025</p>
+      <p>
+        Made by
+        <a href="https://x.com/nnivxix" target="_blank" class="underline"
+          >Hanasa</a
+        >
+      </p>
+    </div>
+  </footer>
+</template>
